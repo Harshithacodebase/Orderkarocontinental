c6a8137332c17659130b49e657b869b29a0ffa68
Harshithacodebase <iharshi242001@gmail.com>
2023-11-03 07:34:16 +0530

    first commit

---
+++ src/test/java/com/orderkarocontinental/OrderkaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderkarocontinental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderkaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
